cb29f8008152933bac9cbc3b9f54370705e6b994
Marcin <mbaranski92@gmail.com>
2019-02-04 11:24:38 +0100

    Dzialajaca wersja konwertera jednostek, na guzikach bez zabezpieczenia błędnego wpisanego typu danych(liter itd).

--- Zadania/src/tydzien2_zadania4x/KonwerterJednostek.java
+++ Zadania/src/tydzien2_zadania4x/KonwerterJednostek.java
@@ -2,17 +2,24 @@ package tydzien2_zadania4x;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
+//import pl.alx.kjava.kalkulator.LogikaKalkulatora;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.Font;
 
 public class KonwerterJednostek {
 
-	private JFrame frame;
+	private JFrame frmKonwerterJednostek;
 	private JTextField txtCm;
 	private JTextField txtM;
 	private JTextField txtKm;
@@ -28,6 +35,7 @@ public class KonwerterJednostek {
 	private JTextField txtMile;
 	private JTextField txtStopy;
 	private JTextField txtCale;
+	private JButton btnZamieNaSystem;
 
 	/**
 	 * Launch the application.
@@ -37,7 +45,7 @@ public class KonwerterJednostek {
 			public void run() {
 				try {
 					KonwerterJednostek window = new KonwerterJednostek();
-					window.frame.setVisible(true);
+					window.frmKonwerterJednostek.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -56,160 +64,277 @@ public class KonwerterJednostek {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		frmKonwerterJednostek = new JFrame();
+		frmKonwerterJednostek.setTitle("Konwerter jednostek");
+		frmKonwerterJednostek.setBounds(100, 100, 450, 247);
+		frmKonwerterJednostek.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		txtCm = new JTextField();
 		txtCm.setColumns(10);
-		
+
 		JLabel lblCm = new JLabel("Centymetry:");
 		lblCm.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+
 		txtM = new JTextField();
 		txtM.setColumns(10);
-		
+
 		JLabel lblMetry = new JLabel("Metry:");
 		lblMetry.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+
 		JLabel lblKilometry = new JLabel("Kilometry:");
 		lblKilometry.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+
 		txtKm = new JTextField();
 		txtKm.setColumns(10);
-		
+
 		txtKg = new JTextField();
 		txtKg.setColumns(10);
-		
+
 		JLabel lblKilogramy = new JLabel("Kilogramy:");
 		lblKilogramy.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+
 		txtStC = new JTextField();
 		txtStC.setColumns(10);
-		
+
 		JLabel lblStopnieCelcjusza = new JLabel("Stopnie Celcjusza:");
 		lblStopnieCelcjusza.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+
 		lblCale = new JLabel("Cale:");
 		lblCale.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+
 		lblStopy = new JLabel("Stopy:");
 		lblStopy.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+
 		lblMile = new JLabel("Mile:");
 		lblMile.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+
 		lblFunty = new JLabel("Funty:");
 		lblFunty.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
-		lblStopnieFarenhaita = new JLabel("Stopnie Farenhaita:");
+
+		lblStopnieFarenhaita = new JLabel("Stopnie \r\nFarenhaita:");
 		lblStopnieFarenhaita.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		
+
 		txtStF = new JTextField();
 		txtStF.setColumns(10);
-		
+
 		txtFunty = new JTextField();
 		txtFunty.setColumns(10);
-		
+
 		txtMile = new JTextField();
 		txtMile.setColumns(10);
-		
+
 		txtStopy = new JTextField();
 		txtStopy.setColumns(10);
-		
+
 		txtCale = new JTextField();
 		txtCale.setColumns(10);
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblKilogramy, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
-										.addComponent(lblStopnieCelcjusza, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-									.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(lblCm)
-										.addPreferredGap(ComponentPlacement.RELATED)))
+
+//		DocumentListener dl = new DocumentListener() {
+//
+//			@Override
+//			public void removeUpdate(DocumentEvent e) {
+//				dzialaj();
+//			}
+//
+//			public void insertUpdate(DocumentEvent e) {
+//				dzialaj();
+//			}
+//
+//			public void changedUpdate(DocumentEvent e) {
+//				dzialaj();
+//			}
+//		};
+
+		JButton btnZamieJednostki = new JButton("Zamień na system brytyjski");
+		btnZamieJednostki.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				dzialaj(0);
+
+			}
+
+		});
+		btnZamieJednostki.setFont(new Font("Tahoma", Font.PLAIN, 13));
+
+		btnZamieNaSystem = new JButton("Zamień na system metryczny");
+		btnZamieNaSystem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dzialaj(1);
+			}
+		});
+		btnZamieNaSystem.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		GroupLayout groupLayout = new GroupLayout(frmKonwerterJednostek.getContentPane());
+		groupLayout
+				.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup().addContainerGap().addGroup(groupLayout
+								.createParallelGroup(
+										Alignment.LEADING)
 								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblKilometry, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)))
-							.addPreferredGap(ComponentPlacement.RELATED, 8, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblMetry, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)))
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(txtCm, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtM, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtKm, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtStC, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtKg, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE))
-					.addGap(36)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblCale, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblStopy, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblMile, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblFunty, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblStopnieFarenhaita, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(txtCale, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtStopy, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtMile, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtFunty, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtStF, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap())
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap(30, Short.MAX_VALUE)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblKilogramy)
-							.addGap(18)
-							.addComponent(lblStopnieCelcjusza))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblCale)
-							.addGap(14)
-							.addComponent(lblStopy)
-							.addGap(15)
-							.addComponent(lblMile)
-							.addGap(15)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblFunty)
-								.addComponent(txtKg, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addGap(15)
-							.addComponent(lblStopnieFarenhaita))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(txtCale, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(11)
-							.addComponent(txtStopy, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(11)
-							.addComponent(txtMile, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(11)
-							.addComponent(txtFunty, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(11)
-							.addComponent(txtStF, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(txtCm, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblCm))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(txtM, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblMetry))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(txtKm, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblKilometry))
-							.addGap(42)
-							.addComponent(txtStC, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(87, Short.MAX_VALUE))
-		);
-		frame.getContentPane().setLayout(groupLayout);
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addComponent(lblKilogramy, GroupLayout.PREFERRED_SIZE, 61,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(lblStopnieCelcjusza, GroupLayout.DEFAULT_SIZE, 119,
+														Short.MAX_VALUE)
+												.addComponent(lblCm)
+												.addComponent(lblKilometry, GroupLayout.PREFERRED_SIZE, 61,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(lblMetry, GroupLayout.PREFERRED_SIZE, 61,
+														GroupLayout.PREFERRED_SIZE))
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addComponent(txtCm, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(txtM, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(txtKm, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(txtStC, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(txtKg, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE))
+										.addGap(27)
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addGroup(groupLayout.createSequentialGroup().addGap(18)
+														.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+																.addComponent(lblMile, GroupLayout.PREFERRED_SIZE, 61,
+																		GroupLayout.PREFERRED_SIZE)
+																.addComponent(lblStopy, GroupLayout.PREFERRED_SIZE, 61,
+																		GroupLayout.PREFERRED_SIZE)
+																.addComponent(lblCale, GroupLayout.PREFERRED_SIZE, 61,
+																		GroupLayout.PREFERRED_SIZE)
+																.addComponent(lblFunty, GroupLayout.PREFERRED_SIZE, 61,
+																		GroupLayout.PREFERRED_SIZE)))
+												.addGroup(groupLayout.createSequentialGroup()
+														.addPreferredGap(ComponentPlacement.RELATED)
+														.addComponent(
+																lblStopnieFarenhaita, GroupLayout.PREFERRED_SIZE, 116,
+																GroupLayout.PREFERRED_SIZE)))
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addComponent(txtCale, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(txtStopy, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(txtMile, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(txtFunty, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(txtStF, GroupLayout.PREFERRED_SIZE, 72,
+														GroupLayout.PREFERRED_SIZE)))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(btnZamieJednostki, GroupLayout.PREFERRED_SIZE, 195,
+												GroupLayout.PREFERRED_SIZE)
+										.addGap(18).addComponent(btnZamieNaSystem, GroupLayout.DEFAULT_SIZE, 201,
+												Short.MAX_VALUE)))
+								.addContainerGap()));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addGroup(groupLayout.createSequentialGroup().addComponent(lblKilogramy).addGap(18)
+										.addComponent(lblStopnieCelcjusza))
+								.addGroup(groupLayout
+										.createSequentialGroup()
+										.addComponent(
+												txtKg, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+												GroupLayout.PREFERRED_SIZE)
+										.addGap(31))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+												.addComponent(txtCale, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+												.addComponent(lblCale))
+										.addGap(11)
+										.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+												.addComponent(txtStopy, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+												.addComponent(lblStopy))
+										.addGap(11)
+										.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+												.addComponent(txtMile, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+												.addComponent(lblMile))
+										.addGap(11)
+										.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+												.addComponent(txtFunty, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+												.addComponent(lblFunty))
+										.addGap(11)
+										.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+												.addComponent(txtStF, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+												.addComponent(lblStopnieFarenhaita)))
+								.addGroup(
+										groupLayout.createSequentialGroup()
+												.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+														.addComponent(txtCm, GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+														.addComponent(lblCm))
+												.addPreferredGap(ComponentPlacement.UNRELATED)
+												.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+														.addComponent(txtM, GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+														.addComponent(lblMetry))
+												.addPreferredGap(ComponentPlacement.UNRELATED)
+												.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+														.addComponent(txtKm, GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+														.addComponent(lblKilometry))
+												.addGap(42).addComponent(txtStC, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(btnZamieJednostki)
+								.addComponent(btnZamieNaSystem, GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGap(52)));
+		frmKonwerterJednostek.getContentPane().setLayout(groupLayout);
+
+//		txtCm.getDocument().addDocumentListener(dl);
+//		txtM.getDocument().addDocumentListener(dl);
+//		txtKm.getDocument().addDocumentListener(dl);
+//		txtKg.getDocument().addDocumentListener(dl);
+//		txtStC.getDocument().addDocumentListener(dl);
+//
+//		txtCale.getDocument().addDocumentListener(dl);
+//		txtStopy.getDocument().addDocumentListener(dl);
+//		txtMile.getDocument().addDocumentListener(dl);
+//		txtFunty.getDocument().addDocumentListener(dl);
+//		txtStF.getDocument().addDocumentListener(dl);
+
 	}
+
+	// param kierunekKonwersji = 0 -> przeliczenie na system brytyjski
+	// param kierunekKonwersji = 1 -> przeliczenie na system metryczny
+	private void dzialaj(int kierunekKonwersji) {
+
+		if (kierunekKonwersji == 0) {
+
+			BigDecimal cm = new BigDecimal((txtCm.getText()));
+			BigDecimal m = new BigDecimal((txtM.getText()));
+			BigDecimal km = new BigDecimal((txtKm.getText()));
+			BigDecimal kg = new BigDecimal((txtKg.getText()));
+			BigDecimal stC = new BigDecimal((txtStC.getText()));
+
+			txtCale.setText("" + LogikaKonwertera.przeliczJednostke(cm, "cm"));
+			txtStopy.setText("" + LogikaKonwertera.przeliczJednostke(m, "m"));
+			txtMile.setText("" + LogikaKonwertera.przeliczJednostke(km, "km"));
+			txtFunty.setText("" + LogikaKonwertera.przeliczJednostke(kg, "kg"));
+			txtStF.setText("" + LogikaKonwertera.przeliczJednostke(stC, "stCelcjusza"));
+
+		} else {
+
+			BigDecimal cale = new BigDecimal((txtCale.getText()));
+			BigDecimal stopy = new BigDecimal((txtStopy.getText()));
+			BigDecimal mile = new BigDecimal((txtMile.getText()));
+			BigDecimal funty = new BigDecimal((txtFunty.getText()));
+			BigDecimal stF = new BigDecimal((txtStF.getText()));
+
+			txtCm.setText("" + LogikaKonwertera.przeliczJednostke(cale, "cale"));
+			txtM.setText("" + LogikaKonwertera.przeliczJednostke(stopy, "stopy"));
+			txtKm.setText("" + LogikaKonwertera.przeliczJednostke(mile, "mile"));
+			txtKg.setText("" + LogikaKonwertera.przeliczJednostke(funty, "funty"));
+			txtStC.setText("" + LogikaKonwertera.przeliczJednostke(stF, "stFarenhaita"));
+		}
+
+	}
+
 }
--- Zadania/src/tydzien2_zadania4x/LogikaKonwertera.java
+++ Zadania/src/tydzien2_zadania4x/LogikaKonwertera.java
@@ -1,21 +1,24 @@
 package tydzien2_zadania4x;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class LogikaKonwertera {
 
-	public static double przeliczJednostke(double iloscJednostkaWejsciowa, String jednostkaWejsciowa) {
+	public static BigDecimal przeliczJednostke(BigDecimal iloscJednostkaWejsciowa, String jednostkaWejsciowa) {
 
 		switch (jednostkaWejsciowa) {
-		case "cm": return iloscJednostkaWejsciowa * 0.39;
-		case "m":  return iloscJednostkaWejsciowa * 3.28;
-		case "km": return iloscJednostkaWejsciowa * 0.62;
-		case "kg": return iloscJednostkaWejsciowa * 2.2;
-		case "stCelcjusza": return iloscJednostkaWejsciowa * 33.8;
-		case "cale": return iloscJednostkaWejsciowa / 0.39;
-		case "stopy":return iloscJednostkaWejsciowa / 3.28;
-		case "mile": return iloscJednostkaWejsciowa / 0.62;
-		case "funty":return iloscJednostkaWejsciowa / 2.2;
-		case "stFarenhaita":return iloscJednostkaWejsciowa / 33.8;
-		default: return 0.0;
+		case "cm": return iloscJednostkaWejsciowa.multiply(BigDecimal.valueOf(0.39)).setScale(3,RoundingMode.HALF_UP);
+		case "m":  return iloscJednostkaWejsciowa.multiply(BigDecimal.valueOf(3.28)).setScale(3,RoundingMode.HALF_UP);
+		case "km": return iloscJednostkaWejsciowa.multiply(BigDecimal.valueOf(0.62)).setScale(3,RoundingMode.HALF_UP);
+		case "kg": return iloscJednostkaWejsciowa.multiply(BigDecimal.valueOf(2.2)).setScale(3,RoundingMode.HALF_UP);
+		case "stCelcjusza": return iloscJednostkaWejsciowa.multiply(BigDecimal.valueOf(33.8)).setScale(3,RoundingMode.HALF_UP);
+		case "cale": return iloscJednostkaWejsciowa.divide(BigDecimal.valueOf(0.39),3,RoundingMode.HALF_UP);
+		case "stopy":return iloscJednostkaWejsciowa.divide(BigDecimal.valueOf(3.28),3,RoundingMode.HALF_UP);
+		case "mile": return iloscJednostkaWejsciowa.divide(BigDecimal.valueOf(0.62),3,RoundingMode.HALF_UP);
+		case "funty":return iloscJednostkaWejsciowa.divide(BigDecimal.valueOf(2.2),3,RoundingMode.HALF_UP);
+		case "stFarenhaita":return iloscJednostkaWejsciowa.divide(BigDecimal.valueOf(33.8),3,RoundingMode.HALF_UP);
+		default: return BigDecimal.valueOf(0.0);
 		}
 
 
